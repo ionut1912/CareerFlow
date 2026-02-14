92ea707cccad98a7b1185bb9afec134facf4803c
nites <nitescucornelionut19@gmail.com>
2026-02-14 22:09:47 +0200

    feat: Update CareerFlow API and infrastructure
    - Added Microsoft.VisualStudio.Azure.Containers.Tools.Targets package to the API project.
    - Updated Dockerfile to correct project paths for Rabbit Events and Infrastructure.
    - Enhanced appsettings.json with JWT settings, connection strings, authentication, RabbitMQ, and Postmark settings.
    - Modified ApplicationDbContextModelSnapshot to enforce string length constraints on the Name property.
    - Removed obsolete Rabbit Events project files.
    - Created new migration files for database schema changes, including updates to the Accounts and LegalDocs tables.
    - Introduced docker-compose.infra.yml for managing PostgreSQL, RabbitMQ, and Redis services with health checks.
    - Added docker-compose.yml to define the CareerFlow API service and its dependencies.

---
+++ CareerFlow.Core/src/CareerFlow.Core.Infrastructure/Migrations/20260214180720_NewChanges.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CareerFlow.Core.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
