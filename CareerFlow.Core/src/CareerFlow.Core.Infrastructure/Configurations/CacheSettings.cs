590ec9f6c346a6a2aa4e60986be03767a14038a0
ionut1912 <nitescucornelionut19@gmail.com>
2026-04-16 16:08:53 +0300

    fix default instance

--- CareerFlow.Core/src/CareerFlow.Core.Infrastructure/Configurations/CacheSettings.cs
+++ CareerFlow.Core/src/CareerFlow.Core.Infrastructure/Configurations/CacheSettings.cs
@@ -5,7 +5,7 @@ public class CacheSettings
     public const string SectionName = "Redis";
 
     public string ConnectionString { get; init; } = string.Empty;
-    public string InstanceName { get; init; } = "MyApp:";
+    public string InstanceName { get; init; } = "careerFlow:";
     public int DefaultExpiryMinutes { get; init; } = 60;
     public bool AbortOnConnectFail { get; init; } = false;
 }
