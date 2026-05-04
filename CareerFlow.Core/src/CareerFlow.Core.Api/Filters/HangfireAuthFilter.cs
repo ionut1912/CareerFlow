5ee4fc9a3f1e8f43c4850ad83d8b7f17396223f8
nites <nitescucornelionut19@gmail.com>
2026-05-04 15:25:56 +0300

    test

--- CareerFlow.Core/src/CareerFlow.Core.Api/Filters/HangfireAuthFilter.cs
+++ CareerFlow.Core/src/CareerFlow.Core.Api/Filters/HangfireAuthFilter.cs
@@ -7,7 +7,6 @@ public sealed class HangfireAuthFilter : IDashboardAuthorizationFilter
     public bool Authorize(DashboardContext context)
     {
         HttpContext? http = context.GetHttpContext();
-        
         return http.User.Identity?.IsAuthenticated == true;
     }
 }
